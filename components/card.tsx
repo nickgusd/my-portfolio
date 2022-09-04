be8c527fc3f9c0941c6dfc45a70e722d466c942d
Nick Guimarais <Nick.guimarais@gmail.com>
2022-09-04 00:53:21 -0700

    styling changes

--- components/card.tsx
+++ components/card.tsx
@@ -4,8 +4,8 @@ import LinkedinSoft from '../public/assets/LinkedinSoft.png';
 
 import styles from './card.module.css';
 
-export const Card = ({ header, content, footer, src }) => (
-  <div className={styles.container}>
+export const Card = ({ header, content, footer, src, onMouseEnter, onMouseLeave }) => (
+  <div className={styles.container} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
     {header && <header>{header}</header>}
     <Image className={styles.image} src={src} width="225" height="225" layout="fixed" />
     {content && <div>{content}</div>}
--- components/contact.tsx
+++ components/contact.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 
 import styles from './contact.module.css';
@@ -8,14 +8,22 @@ import { Card } from './card';
 
 export const Contact = () => {
   const handleMail = () => window.open(`mailto:nick.guimarais@gmail.com`);
+  const [active, setActive] = useState(null);
+  const handleMouseEnter = () => setActive(true);
+  const handleMouseLeave = () => setActive(false);
+
   return (
     <div className={styles.wrapper}>
       <Card
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
         src={picture}
         header={null}
         content={
           <div className={styles.content}>
-            <p onClick={handleMail}>Email: nick.guimarais@gmail.com</p>
+            <p onClick={handleMail} className={active ? styles.link : styles.inactive}>
+              <span>Email:</span> nick.guimarais@gmail.com
+            </p>
             <p>Phone: (818) 235-8076</p>
           </div>
         }
