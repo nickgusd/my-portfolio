d2b593f5e5f0bdb245f5b9b05a10eb8259410312
Nick Guimarais <Nick.guimarais@gmail.com>
2022-09-09 14:57:06 -0700

    added contact page for mobile

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,20 +1,21 @@
-import Image from 'next/image';
+import { useMediaQuery } from 'react-responsive';
 
 import Layout from '../components/layout';
 import { About } from '../components/about';
 import { Contact } from '../components/contact';
 import { Sidebar } from '../components/sidebar';
-import LogoIcon from '../public/assets/Logo-icon.png';
 
 import styles from '../components/about.module.css';
 
 export default function Home() {
+  const isMobile = useMediaQuery({ query: '(max-width: 820px)' });
+
   return (
     <Layout>
       <div className={styles.container}>
         <Sidebar />
         <About />
-        <Contact />
+        {!isMobile && <Contact />}
       </div>
     </Layout>
   );
