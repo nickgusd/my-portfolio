d23922a11859f4e35377ada31a2777c657d246bd
Nick Guimarais <Nick.guimarais@gmail.com>
2022-09-22 17:44:49 -0700

    restore resume page

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-// import dynamic from 'next/dynamic';
+import dynamic from 'next/dynamic';
 
-// const PDFViewer = dynamic(() => import('../components/pdfviewer'), {
-//   ssr: false
-// });
+const PDFViewer = dynamic(() => import('../components/pdfviewer'), {
+  ssr: false
+});
 
 export default function Resume(): JSX.Element {
   return (
     <div className="res">
-      {/* <PDFViewer /> */}
-      <h1>Resume page test</h1>
-      <p>This page should not be a 404</p>
+      <PDFViewer />
     </div>
   );
 }
