ba65348d76e9665ede8846af63db6ce391ec5200
Nick Guimarais <Nick.guimarais@gmail.com>
2022-12-10 14:47:16 -0800

    re-deployed apps that crashed

--- content/projects.js
+++ content/projects.js
@@ -8,6 +8,14 @@
         "A job search platform that allows a user to search for remote and hybrid jobs only"
     },
     {
+        project: "Ernie Ball Landing Page",
+        github: "https://github.com/nickgusd/Landing-Page",
+        deployed: "https://landing-page-nickgusd.vercel.app/", 
+        image: "/apple.png",
+        description: 
+        "This website is a product landing page inspired by my favorite guitar series, the Ernie Ball Music Man!"
+    },
+    {
          project: "Financial Calculator",
          github: "https://github.com/nickgusd/PV-FV-Calculator",
          deployed: "http://financial-calculator-react.s3-website-us-west-1.amazonaws.com/",
@@ -15,13 +23,21 @@
          description: 
              "This is a functional Financial Calculator that allows you to perform Present Value, Future Value, Payments, Interest Rate, and Number of periods calculations."
      },
-     {
-        project: "Ernie Ball Landing Page",
-        github: "https://github.com/nickgusd/Landing-Page",
-        deployed: "https://fathomless-basin-76735.herokuapp.com/", 
-        image: "/apple.png",
+    {
+        project: "Tinder for Restaurants",
+        github: "https://github.com/tpacba/tinderforrestaurants",
+        deployed: "https://tinder-for-restaurants.herokuapp.com/", 
+        image: "/ipad.png",
         description: 
-        "This website is a product landing page inspired by my favorite guitar series, the Ernie Ball Music Man!"
+        "An application that helps a group of people choose a restaurant through rounds of swiping, similar to Tinder UI. When the group narrows down the restaurants that they like to 1 restaurant, the group is presented with a match."
+    },
+    {
+        project: "Note-Taker",
+        github: "https://github.com/nickgusd/Note-Taker",
+        deployed: "https://note-taker-021.herokuapp.com/",
+        image: "/laptop.png", 
+        description: 
+        "An application that has the ability to save, delete, and view old notes."
     },
     {
         project: "Music eXchange",
@@ -31,22 +47,6 @@
         description: 
         "An application that allows musicians and artists to their sell music and tutorials directly to their fans."
     },
-    {
-        project: "Tinder for Restaurants",
-        github: "https://github.com/tpacba/tinderforrestaurants",
-        deployed: "https://vast-lowlands-43566.herokuapp.com/", 
-        image: "/ipad.png",
-        description: 
-        "An application that helps a group of people choose a restaurant through rounds of swiping, similar to Tinder UI. When the group narrows down the restaurants that they like to 1 restaurant, the group is presented with a match."
-    },
-    {
-        project: "Note-Taker",
-        github: "https://github.com/nickgusd/Note-Taker",
-        deployed: "https://protected-shore-03622.herokuapp.com/",
-        image: "/laptop.png", 
-        description: 
-        "An application that has the ability to save, delete, and view old notes."
-    }
  ]
  
 
