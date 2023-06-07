dd72387344cb3ee90e5c49960988c069a3d23a44
Nick Guimarais <Nick.guimarais@gmail.com>
2023-06-07 10:29:15 -0700

    add Blossm and tooltip to portfolio

--- content/experience.js
+++ content/experience.js
@@ -1,31 +1,54 @@
 export const experience = [
-    {
-        position: "Front End Engineer",
-        company: "Demyst",
-        date: "June 2021 - January 2023",
-        description: "I played a key role in building the UI for the Data API platform, which featured a searchable and filterable Data Catalog, Data APIs, a Billing Dashboard, a Team Settings page, and the ability to generate sample Data APIs on the spot using a selected dataset. In addition, I designed the UI for various client projects, including Banker's portals, which streamlined the lending process for small businesses by allowing them to upload financial statements and receive automatic lending decisions based on the bank's requirements. I also contributed to creating and maintaining reusable components for the Demyst Design System in Storybook, which was used to build both the Data API platform and client projects. Moreover, I helped implement new features, resolve bugs, and facilitate the migration of the platform to Next.js.",
-        techStack: "JavaScript, Next.js, React, HTML, CSS, Redux, AWS, Styled Components, Less, Axios, Jenkins CI/CD, REST APIs, Recharts, Storybook",
-        tools: "Jira, Github Issues, Figma",
-        testing: "Cypress, Enzyme, React Testing Library, Jest"
-    },
-    {
-        position: "Front End Engineer",
-        company: "Lifeshack",
-        date: "November 2020 - May 2021",
-        description: 'I played a vital role in developing the UI for Lifeshack.com, a job search platform exclusively designed for remote and hybrid positions. My work involved creating a mobile-responsive interface, implementing job filters for both desktop and mobile versions, designing reusable components, developing profile and preference pages, and integrating GraphQL endpoints on the front end. As part of my contributions, I conducted thorough quality assurance testing on the entire platform to ensure its smooth and efficient functionality.',
-        techStack: "TypeScript, Next.js, React.js, HTML, CSS, UseContext, Node.js, Express, GraphQL, AWS, Apollo, DynamoDB, Semantic UI",
-        tools: "AdobeXD, Github Issues"
-    },
-    {
-        position: "Senior Associate",
-        company: "MGO LLP",
-        date: "July 2019 - April 2020",
-        description: 'Led multiple client engagements, client meetings, performed review of fieldwork, and coordinated management reviews of field work, Arrangement Letters, Consulting Reports, Audit Reports, and Rep letters. Performed internal control audits over client’s IT Department, and interviewed the CTO and CIO. Performed Financial Statement Audits and review of Financial Statements to ensure they are fairly presented and tie out to the footnotes. Utilized QuickBooks for client’s bookkeeping which included preparing various journal entries, schedules, reconciliations, and preparing monthly Profit and Loss, Balance Sheet, and Statement of Cash Flows.'
-    },
-    {
-        position: "Consulting Associate",
-        company: "RSM US LLP",
-        date: "June 2016 - November 2018",
-        description: 'Traveled to around 50 casinos across the United States to perform fieldwork. Led multiple client engagements, client meetings, performed review of fieldwork, and coordinated management reviews of field work, Arrangement Letters, Consulting Reports, Audit Reports, and Rep letters. Performed risk assessments and provided recommendations over the internal procedures around Cash Operations, Information Technology, Slots, Table Games, Bingo, and Surveillance. Performed Anti-Money Laundering (AML) consulting engagements to improve the client’s Anti-Money Laundering (AML) program, and to help identify suspicious activity related to money laundering.'
-    },
-]
+  {
+    position: 'Frontend Developer - Freelance',
+    company: 'Blossm Inc.',
+    companyDescription:
+      'Blossm is a Community Marketplace to Buy, Sell, and Trade Plants. This plant marketplace is available on IOS, Android, and Web. Blossm has raised funds through Angel Investors and has already been generating revenue.',
+    date: 'April 2023 - Present',
+    description:
+      'I helped revamp the entire website and developed landing pages for Google Ads as well as to help strengthen SEO. I also built a page to market the Blossm plant sellers that displays their sellers profile as well as their plant inventory. Additionally, I have helped implement Google Analytics click conversions all over the website to determine where users are coming from in order to determine the effectiveness of Google Ads.',
+    techStack: 'JavaScript, Next.js, React, Bootstrap, HTML, CSS, Axios, Firebase',
+    tools: 'Google Analytics, Hotjar',
+    testing: null
+  },
+  {
+    position: 'Frontend Developer',
+    company: 'Demyst',
+    companyDescription:
+      'Demyst is a Series E funded data startup providing external data to large enterprises via APIs',
+    date: 'June 2021 - January 2023',
+    description:
+      "I played a key role in building the UI for the Data API platform, which featured a searchable and filterable Data Catalog, Data APIs, a Billing Dashboard, a Team Settings page, and the ability to generate sample Data APIs on the spot using a selected dataset. In addition, I designed the UI for various client projects, including Banker's portals, which streamlined the lending process for small businesses by allowing them to upload financial statements and receive automatic lending decisions based on the bank's requirements. I also contributed to creating and maintaining reusable components for the Demyst Design System in Storybook, which was used to build both the Data API platform and client projects. Moreover, I helped implement new features, resolve bugs, and facilitate the migration of the platform to Next.js.",
+    techStack:
+      'JavaScript, Next.js, React, HTML, CSS, Redux, AWS, Styled Components, Less, Axios, Jenkins CI/CD, REST APIs, Recharts, Storybook',
+    tools: 'Jira, Github Issues, Figma',
+    testing: 'Cypress, Enzyme, React Testing Library, Jest'
+  },
+  {
+    position: 'Frontend Developer',
+    company: 'Lifeshack',
+    companyDescription: 'Lifeshack is a remote and hybrid jobs search platform',
+    date: 'November 2020 - May 2021',
+    description:
+      'I played a vital role in developing the UI for Lifeshack.com, a job search platform exclusively designed for remote and hybrid positions. My work involved creating a mobile-responsive interface, implementing job filters for both desktop and mobile versions, designing reusable components, developing profile and preference pages, and integrating GraphQL endpoints on the front end. As part of my contributions, I conducted thorough quality assurance testing on the entire platform to ensure its smooth and efficient functionality.',
+    techStack:
+      'TypeScript, Next.js, React.js, HTML, CSS, UseContext, Node.js, Express, GraphQL, AWS, Apollo, DynamoDB, Semantic UI',
+    tools: 'AdobeXD, Github Issues'
+  },
+  {
+    position: 'Senior Associate',
+    company: 'MGO LLP',
+    companyDescription: 'MGO is a national middle-market Accounting/Consulting firm',
+    date: 'July 2019 - April 2020',
+    description:
+      'Led multiple client engagements, client meetings, performed review of fieldwork, and coordinated management reviews of field work, Arrangement Letters, Consulting Reports, Audit Reports, and Rep letters. Performed internal control audits over client’s IT Department, and interviewed the CTO and CIO. Performed Financial Statement Audits and review of Financial Statements to ensure they are fairly presented and tie out to the footnotes. Utilized QuickBooks for client’s bookkeeping which included preparing various journal entries, schedules, reconciliations, and preparing monthly Profit and Loss, Balance Sheet, and Statement of Cash Flows.'
+  },
+  {
+    position: 'Consulting Associate',
+    company: 'RSM US LLP',
+    companyDescription: 'RSM is the 5th largest Accounting/Consulting firm in the United States',
+    date: 'June 2016 - November 2018',
+    description:
+      'Traveled to around 50 casinos across the United States to perform fieldwork. Led multiple client engagements, client meetings, performed review of fieldwork, and coordinated management reviews of field work, Arrangement Letters, Consulting Reports, Audit Reports, and Rep letters. Performed risk assessments and provided recommendations over the internal procedures around Cash Operations, Information Technology, Slots, Table Games, Bingo, and Surveillance. Performed Anti-Money Laundering (AML) consulting engagements to improve the client’s Anti-Money Laundering (AML) program, and to help identify suspicious activity related to money laundering.'
+  }
+];
